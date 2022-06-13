2db988d003a302de8371e1e9caa698a89cf245ec
Adél Juhasz <sz0juhade@vasvari.org>
2022-06-13 15:21:41 +0200

    create footer component

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+import logo from "../../images/next-level-logo.webp";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <img src={logo} alt="next level logo" />
+        </footer>
+    ) 
+}
