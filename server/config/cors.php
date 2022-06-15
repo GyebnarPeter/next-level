a51482116f13e4f81494810afa3d9db7ac976a0a
denathe <attila.dencsik95@gmail.com>
2022-06-15 10:18:03 +0200

    cors working in dev mode

--- server/config/cors.php
+++ server/config/cors.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
 
     /*
@@ -29,6 +28,7 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, //default option is false
 
 ];
+
