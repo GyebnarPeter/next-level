bbc6789faaceab1a641f235905ae5edaa9e3b0fe
GyebnarPeter <gyebnarpetter@gmail.com>
2022-06-20 17:09:10 +0200

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import UniqueOffers from "./pages/uniqueOffers/UniqueOffers";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
-        <div className="App">
+        <Router>
             <Header />
-            <Home />
-        </div>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/rolunk" element={<About />} />
+                <Route exact path="/egyedi-ajanlatok" element={<UniqueOffers />} />
+                <Route exact path="/kapcsolat" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </Router>
     );
 }
 
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,54 +1,62 @@
-import { useState } from "react";
 import "./header.css";
+import { useState } from "react";
 import logo from "../../images/next-level-logo.webp";
 import mobileLogo from "../../images/next-level-mobile-logo.webp";
 import navCloseButton from "../../images/close.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+    const [openNav, setOpenNav] = useState(false);
 
-  const [openNav, setOpenNav] = useState(false);
-
-  return (
-    <header className="header">
-        <div className="container">
-            <div className="headerLogos">
-                <img 
-                    src={logo} 
-                    className="headerLogo" 
-                    alt="next level logo" 
-                />
-                <img 
-                    src={mobileLogo} 
-                    className="headerLogoMobile" 
-                    alt="next level logo" 
-                />
-            </div>
-            <nav 
-                className="nav"
-                style={ openNav ? {transform: 'initial'} : {transform: 'translateX(100%)'}}
-            >
-                <div className="navClose">
-                    <img 
-                        src={navCloseButton} 
-                        className="navCloseButton" 
-                        alt="bezar" 
-                        onClick={ () => setOpenNav(!openNav) } 
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="headerLogos">
+                    <img
+                        src={logo}
+                        className="headerLogo"
+                        alt="next level logo"
+                    />
+                    <img
+                        src={mobileLogo}
+                        className="headerLogoMobile"
+                        alt="next level logo"
                     />
                 </div>
-                <ul className="navList">
-                    <li className="navListItem"><a href="" className="navLink link">Főoldal</a></li>
-                    <li className="navListItem"><a href="" className="navLink link">Játékok</a></li>
-                    <li className="navListItem"><a href="" className="navLink link">Rólunk</a></li>
-                    <li className="navListItem"><a href="" className="navLink link">Egyedi ajánlatok</a></li>
-                    <li className="navListItem"><a href="" className="navLink link">Kapcsolat</a></li>
-                </ul>
-            </nav>
-            <div className="navBtn" onClick={ () => setOpenNav(!openNav) }>
-                <span></span>
-                <span></span>
-                <span></span>
+                <nav
+                    className="nav"
+                    style={
+                        openNav
+                            ? { transform: "initial" }
+                            : { transform: "translateX(100%)" }
+                    }
+                >
+                    <div className="navClose">
+                        <img
+                            src={navCloseButton}
+                            className="navCloseButton"
+                            alt="bezar"
+                            onClick={() => setOpenNav(!openNav)}
+                        />
+                    </div>
+                    <ul className="navList">
+                        <li className="navListItem"><Link to="/">Föoldal</Link></li>
+                        <li className="navListItem">
+                            <a href="" className="navLink link">
+                                Hasonmás
+                            </a>
+                        </li>
+                        <li className="navListItem"><Link to="rolunk">Rólunk</Link></li>
+                        <li className="navListItem"><Link to="egyedi-ajanlatok">Egyedi ajánlatok</Link></li>
+                        <li className="navListItem link"><Link to="kapcsolat">Kapcsolat</Link></li>
+                    </ul>
+                </nav>
+                <div className="navBtn" onClick={() => setOpenNav(!openNav)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    );
 }
