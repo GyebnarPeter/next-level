0af341a6bd227441e55e2a4b0b73f6c8e121a219
Adél Juhasz <sz0juhade@vasvari.org>
2022-06-20 14:32:01 +0200

    create counterDescription

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Home from "./pages/home/Home";
 import UniqueOffers from "./pages/uniqueOffers/UniqueOffers";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
+import Counterpart from "./pages/counterpart/Counterpart";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Contact />
+            <Counterpart />
         </div>
     );
 }
