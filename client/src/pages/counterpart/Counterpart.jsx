cbb400de5ee52cf5b18773752ec8d8de5fe79963
GyebnarPeter <gyebnarpetter@gmail.com>
2022-06-21 16:45:12 +0200

    install day time picker

--- client/src/pages/counterpart/Counterpart.jsx
+++ client/src/pages/counterpart/Counterpart.jsx
@@ -1,14 +1,18 @@
 import CounterpartDescription from "../../components/counterpartDescription/CounterpartDescription";
 import GameIntro from "../../components/gameIntro/GameIntro";
+import Rules from "../../components/rules/Rules";
+import Calend from "../../components/calend/Calend";
+
+// Import images
 import counterpart from "../../images/hasonmas-logo.webp";
 import background from "../../images/hasonmas_bg.webp";
-import Rules from "../../components/rules/Rules";
 
 export default function Counterpart() {
     return (
         <>
-            <GameIntro src={counterpart} href="/" background={background} />
+            <GameIntro src={ counterpart } href="/" background={ background } />
             <CounterpartDescription />
+            <Calend />
             <Rules />
         </>
     );
