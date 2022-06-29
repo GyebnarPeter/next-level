ab68d391c62e1d288b959d847cb53f32f899d472
GyebnarPeter <gyebnarpetter@gmail.com>
2022-06-29 16:44:30 +0200

    send image to server test

--- client/src/components/booking/ImageUploader.jsx
+++ client/src/components/booking/ImageUploader.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "./imageUploader.css";
 import remove from "../../images/close-blue.webp";
+const axios = require("axios").default;
 
 function ImageUploader() {
+    /* const image = require("../../images/white-bg.webp"); */
+
+    const uploadHandler = (e) => {
+      const url = "http://localhost:8000/api/add-temp";
+      const image = e.target.files[0];
+
+      axios
+          .post(url, image)
+          .then(function(response) {
+            console.log(response);
+          })
+          .catch(function(error) {
+              console.log(error);
+          });
+    }
+
     return (
         <div className="imageUploader">
-            <input type="file" className="image" />
+            <input type="file" className="image" onChange={ uploadHandler } />
             <div className="upload">
                 <h4>Feltöltött kép</h4>
                 <div className="uploaded-image">
-                    <img src="" alt="" onError={(event) => event.target.style.display = 'none'}/>
+                    <img
+                        src=""
+                        alt=""
+                        onError={(event) =>
+                            (event.target.style.display = "none")
+                        }
+                    />
                     <span className="removeImageBtn">
                         <img src={remove} alt="torles" />
                     </span>
